ae3aff752693bae9f818ded37a41a175a1a0b97e
Liam Deacon <liam.deacon@lightbytestechnology.co.uk>
2022-05-15 16:51:14 +0100

    Add test skeleton

---
+++ tests/test_routes.py
@@ -0,0 +1,25 @@
+import pytest
+
+from app.db.repositories import AnalystRepository
+
+
+@pytest.mark.analyst
+def test_create_analyst(test_client) -> None:
+    ...
+
+
+@pytest.mark.analyst
+def test_read_analyst(test_client):
+    ...
+
+
+@pytest.mark.analyst
+def test_update_analyst(test_client):
+    ...
+
+
+@pytest.mark.analyst
+def test_delete_analyst(test_client):
+    ...
+
+
