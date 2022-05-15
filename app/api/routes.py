6ec3f4f5c978eb6a882503dbd63a5125d64bc7d9
Liam Deacon <liam.deacon@lightbytestechnology.co.uk>
2022-05-15 15:50:54 +0100

    Implement endpoints using repository pattern

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,6 @@
 import os
 
-from typing import List
+from typing import Any, List
 
 from fastapi import FastAPI
 from fastapi_async_sqlalchemy import SQLAlchemyMiddleware, db
@@ -11,69 +11,101 @@ from starlette import status
 from .schemas import AnalystInputSchema, AssetInputSchema, AssetSchema, AnalystSchema
 from ..db.tables import AssetModel, AnalystModel
 from ..db.repositories import AssetRepository, AnalystRepository
+from ..db.utils import setup_database
 
-#: Defines the database connection URL, defaulting to in-memory SQLite DB
+#: Defines the database connection URL, defaulting to local SQLite DB
 sqlalchemy_connection_string: str = \
-    os.environ.get('SQLALCHEMY_DATABASE_CONNECTION_URI') or 'sqlite+aiosqlite://'  # noqa: E501
+    os.environ.get('SQLALCHEMY_DATABASE_CONNECTION_URI') or 'sqlite+aiosqlite:///db.sqlite'  # noqa: E501
 
-app = FastAPI()
+# TODO: enabled in-memory database support (unable to create tables using sync + async drivers)
+setup_database(sqlalchemy_connection_string)
+
+app = FastAPI(title="Icy Morning REST API")
 
 app.add_middleware(SQLAlchemyMiddleware, db_url=sqlalchemy_connection_string)
 
 
 api_prefix = '/api/v1'
 
-assets_repo = AssetRepository(db)
-analysts_repo = AnalystRepository(db)
+assets_repo = AssetRepository()
+analysts_repo = AnalystRepository()
 
 
-@app.get(f'{api_prefix}/assets')
+@app.get(f'{api_prefix}/assets', response_model=List[AssetSchema])
 async def get_assets():
+    """Retrieves all assets."""
     assets: List[AssetModel] = await db.session.query(AssetModel).all()
     return [AssetSchema.from_orm(asset) for asset in assets]
 
 
-@app.post(f'{api_prefix}/assets', status_code=status.HTTP_201_CREATED)
+@app.post(f'{api_prefix}/assets',
+          status_code=status.HTTP_201_CREATED,
+          response_model=AssetSchema)
 async def create_asset(payload: AssetSchema):
-    return await assets_repo.create(payload)
+    """Create a new asset."""
+    asset = await assets_repo.create(payload)
+    return asset
 
 
-@app.get(f'{api_prefix}/assets/{{asset_id}}')
+@app.get(f'{api_prefix}/assets/{{asset_id}}', response_model=AssetSchema)
 async def read_asset(asset_id: int):
-    return await assets_repo.read(asset_id)
+    """Retrieve data on a specific asset given by `asset_id`."""
+    asset = await assets_repo.read(asset_id)
+    return asset
 
 
-@app.put(f'{api_prefix}/assets/{{asset_id}}')
+@app.put(f'{api_prefix}/assets/{{asset_id}}', response_model=AssetSchema)
 async def update_asset(asset_id: int, payload: AssetInputSchema):
-    return await assets_repo.update(asset_id, payload)
+    """Update information on asset given by `asset_id`."""
+    asset = await assets_repo.update(asset_id, payload)
+    return asset
 
 
-@app.delete(f'{api_prefix}/assets/{{asset_id}}')
+@app.delete(f'{api_prefix}/assets/{{asset_id}}', response_model=AssetSchema)
 async def delete_asset(asset_id: int):
-    return await assets_repo.delete(asset_id)
+    """Delete asset from database."""
+    asset = await assets_repo.delete(asset_id)
+    return asset
 
 
-@app.get(f'{api_prefix}/analysts')
+@app.get(f'{api_prefix}/analysts', response_model=List[AnalystSchema])
 async def get_analysts():
+    """Retrieve list of analysts."""
     analysts: List[AnalystModel] = await db.session.query(AnalystModel).all()
     return [AnalystSchema(analyst) for analyst in analysts]
 
 
-@app.post(f'{api_prefix}/analysts', status_code=status.HTTP_201_CREATED)
+@app.post(f'{api_prefix}/analysts',
+          status_code=status.HTTP_201_CREATED,
+          response_model=AnalystSchema)
 async def create_analyst(payload: AnalystInputSchema):
-    return await analysts_repo.create(payload)
+    """Add a new analyst to the database."""
+    analyst = await analysts_repo.create(payload)
+    return analyst
 
 
-@app.get(f'{api_prefix}/analysts/{{analyst_id}}')
+@app.get(f'{api_prefix}/analysts/{{analyst_id}}', response_model=AnalystSchema)
 async def get_analyst_by_id(analyst_id: int):
-    return await analysts_repo.read(analyst_id) 
+    """Retrieve data on a specific analyst given by `analyst_id`."""
+    analyst = await analysts_repo.read(analyst_id)
+    return analyst
 
 
-@app.put(f'{api_prefix}/analysts/{{analyst_id}}')
+@app.put(f'{api_prefix}/analysts/{{analyst_id}}', response_model=AnalystSchema)
 async def update_analyst(analyst_id: int, payload: AnalystSchema):
-    return await analysts_repo.update(analyst_id, payload)
+    """Update analyst information.
 
 
-@app.delete(f'{api_prefix}/analysts/{{analyst_id}}')
+    ## Notes
+
+    The asset information must be complete - partial information update is not supported.    
+    """
+    analyst = await analysts_repo.update(analyst_id, payload)
+    return analyst
+
+
+@app.delete(f'{api_prefix}/analysts/{{analyst_id}}', response_model=AnalystSchema)
 async def delete_analyst(analyst_id: int):
-    return await analysts_repo.delete(analyst_id)
+    """Delete analyst from database."""
+    analyst = await analysts_repo.delete(analyst_id)
+    return analyst
