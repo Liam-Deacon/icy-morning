c067ceeb330cf2fa887d252f78937203fafe16eb
Liam Deacon <liam.deacon@lightbytestechnology.co.uk>
2022-05-20 15:09:56 +0100

    Fix s3 ids

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "lambda_bucket" {
 
 resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
   # enabled = true
-  bucket = aws_s3_bucket.lambda_bucket
+  bucket = aws_s3_bucket.lambda_bucket.id
 
   versioning_configuration {
     status = "Enabled"
@@ -30,12 +30,12 @@ resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
 }
 
 resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
-  bucket = aws_s3_bucket.lambda_bucket
+  bucket = aws_s3_bucket.lambda_bucket.id
   acl    = "private"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "lambda_bucket_encryption" {
-  bucket = aws_s3_bucket.lambda_bucket
+  bucket = aws_s3_bucket.lambda_bucket.id
   rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
